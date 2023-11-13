9a48f00df5233d77a2d14dd00897918e20721f2b
Suhas Jagtap <jagtapsuhas647@gmail.com>
2023-11-13 12:34:44 +0100

    Add requirements.txt, setup.py, and __init__.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+       'numpy==1.21.0', # List your project dependencies here
+    ],
+)
